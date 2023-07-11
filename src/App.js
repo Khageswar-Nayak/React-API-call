685f0713b19a4dc0734ccb425939ba79f857309e
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-07-11 22:26:13 +0530

    Adding new Movies intio the list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -48,6 +49,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  const addMovieHandler = (NewMovieObj) => {
+    console.log(NewMovieObj);
+  };
+
   function handleCancelRetry() {
     // Clear the retry timeout and reset error state
     clearTimeout(retryTimeout);
@@ -71,6 +76,9 @@ function App() {
   }
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
